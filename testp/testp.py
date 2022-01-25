7a64e23c43573cc33fc5e396f97cfe46e8fe4c5a
dkukeijiy <87068908+dkukeijiy@users.noreply.github.com>
2022-01-25 13:03:30 +0900

    Update testp.py

--- testp/testp.py
+++ testp/testp.py
@@ -1,2 +1,9 @@
+import dataiku
+
+
 def testf():
-  return 1
+  print('start testf()')
+  f = dataiku.Folder("29522.yCKNDeC2")
+  folder_path = f.get_path()
+  print('folder_path:', folder_path)
+  return 2
