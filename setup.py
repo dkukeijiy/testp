35f3e46b066960b9f718fd67818c856e145174ea
dkukeijiy <87068908+dkukeijiy@users.noreply.github.com>
2022-01-25 13:04:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
  
 setuptools.setup(
     name="testp",
-    version="1.0",
+    version="1.1",
     author="testp",
     author_email="testp@example.com",
     description="libhollow is my own python package",
