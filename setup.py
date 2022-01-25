2ea659a1a0fd269f3c2c6314fb94f09756675520
dkukeijiy <87068908+dkukeijiy@users.noreply.github.com>
2022-01-25 12:52:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='testp',
-    version="0.0.1",
-    description="testp",
-    long_description="",
-    author='testpauther',
-    license='MIT',
+import setuptools
+ 
+setuptools.setup(
+    name="testp",
+    version="1.0",
+    author="testp",
+    author_email="testp@example.com",
+    description="libhollow is my own python package",
+    long_description="The halloween party has gone over...",
+    long_description_content_type="text/markdown",
+    url="https://blog.chocolapod.net/momokan",
+    packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 1 - Planning"
+        "Programming Language :: Python :: 2.7.14",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ]
 )
